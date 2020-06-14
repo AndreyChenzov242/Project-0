46f966c7c8d56258f8cdbb35d45fbdb2a553254b
AndreyChenzov242 <andreychenzov@gmail.com>
2020-06-14 21:47:05 +0300

    add burger

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(function(){
 	
 	$('.click').on('click', function(){
 		
-		$('.container-burger').slideToggle(300, function(){
+		$('.container-burger').slideToggle(1, function(){
 			
 			if($(this).css('display') === 'none'){
                 $(this).removeAttr('style');
